781c1b8e61c6ae55a369bedec5e36233dbb3b66c
Pat <hurley.0189@gmail.com>
2020-03-18 13:18:40 -0600

    Initial commit

---
+++ Roll20s/src/diceRoller/DiceRoller.java
@@ -0,0 +1,48 @@
+package diceRoller;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class DiceRoller extends JFrame {
+	private String[] dice = {"D3", "D4", "D6", "D8", "D10", "D20"};
+	private int[] num = {3,4,6,8,10,20};
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DiceRoller frame = new DiceRoller();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public DiceRoller() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JLabel DiceLbl = new JLabel("New label");
+		contentPane.add(DiceLbl, BorderLayout.NORTH);
+	}
+
+}
